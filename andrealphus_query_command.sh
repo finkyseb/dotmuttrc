aded7326784dcd2498a410f729a7cd00d6cb96ec
Virgile Gerecke <virgile.gerecke@unistra.fr>
2015-05-29 07:41:32 +0200

    FirstName comes first

--- andrealphus_query_command.sh
+++ andrealphus_query_command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "`/usr/local/bin/contacts -s -Sf '%e:%ln %fn:%nn' $@ | \\
+echo "`/usr/local/bin/contacts -s -Sf '%e:%fn %ln:%nn' $@ | \\
 awk -F':' '{printf $1 "\t" $2 "\t" $3 "\n"}'`"
